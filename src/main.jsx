4e080a5d06d27a9e2927e8db55e6713f276be5fa
M.Cevat Morçiçek <m.cevatmorcicek@MCevat-MacBook-Air.local>
2026-03-19 05:38:56 +0300

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './ciro-takip';
+import './index.css';
+
+ReactDOM.createRoot(document.getElementById('root')).render(<App />);
